d23a5a5cfce4946917369ee337872b0316f9af19
bahill <bhill@broadinstitute.org>
2021-10-20 02:00:25 -0400

    updating to latest

--- MBTA.py
+++ MBTA.py
@@ -2,5 +2,6 @@ import requests
 
 # can't hurt
 api_key = "6f1058bc4865463e8aba8608821ab32c"
-
+default_params = {"api_key": api_key}
 endpoint_routes = "https://api-v3.mbta.com/routes"
+endpoint_stops = "https://api-v3.mbta.com/stops"
